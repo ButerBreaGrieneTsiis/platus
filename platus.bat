4f09e5482359e4384cf95d6b54981511cf163afa
ButerBreaGrieneTsiis <tomscheeper@gmail.com>
2025-05-04 18:04:44 +0200

    Add files via upload

--- platus.bat
+++ platus.bat
@@ -1,13 +1,13 @@
 @ECHO OFF
 CLS
-ECHO             ,,                                    
-ECHO `7MM"""Mq.`7MM           mm                       
-ECHO   MM   `MM. MM           MM                       
-ECHO   MM   ,M9  MM   ,6"Yb.mmMMmm `7MM  `7MM  ,pP"Ybd 
-ECHO   MMmmdM9   MM  8)   MM  MM     MM    MM  8I   `" 
-ECHO   MM        MM   ,pm9MM  MM     MM    MM  `YMMMa. 
-ECHO   MM        MM  8M   MM  MM     MM    MM  L.   I8 
-ECHO .JMML.    .JMML.`Moo9^Yo.`Mbmo  `Mbod"YML.M9mmmP' 
+ECHO             ,,
+ECHO `7MM"""Mq.`7MM           mm
+ECHO   MM   `MM. MM           MM
+ECHO   MM   ,M9  MM   ,6"Yb.mmMMmm `7MM  `7MM  ,pP"Ybd
+ECHO   MMmmdM9   MM  8)   MM  MM     MM    MM  8I   `"
+ECHO   MM        MM   ,pm9MM  MM     MM    MM  `YMMMa.
+ECHO   MM        MM  8M   MM  MM     MM    MM  L.   I8
+ECHO .JMML.    .JMML.`Moo9^Yo.`Mbmo  `Mbod"YML.M9mmmP'
 ECHO                                      versie 1.2.1
 ECHO.
 ECHO maak een keuze:
